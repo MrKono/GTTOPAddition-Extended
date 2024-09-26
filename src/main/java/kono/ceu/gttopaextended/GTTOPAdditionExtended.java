fe78f95d6a1808b4a1e9091d40f34edf027e8ca2
ko_no <90126004+MrKono@users.noreply.github.com>
2024-09-26 20:04:59 +0900

    init commit

---
+++ src/main/java/kono/ceu/gttopaextended/GTTOPAdditionExtended.java
@@ -0,0 +1,26 @@
+package kono.ceu.gttopaextended;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import gregtech.GTInternalTags;
+
+import gregicality.multiblocks.GregicalityMultiblocks;
+
+import keqing.gttopaddition.GTTOPAddition;
+
+@Mod(modid = Tags.MODID,
+     version = Tags.VERSION,
+     name = Tags.MODNAME,
+     acceptedMinecraftVersions = "[1.12.2]",
+     dependencies = GTInternalTags.DEP_VERSION_STRING + "required-after:" + GTTOPAddition.MODID + ";" +
+             "after:" + GregicalityMultiblocks.MODID + ";")
+public class GTTOPAdditionExtended {
+
+    @Mod.Instance
+    public static GTTOPAdditionExtended instance;
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {}
+}
