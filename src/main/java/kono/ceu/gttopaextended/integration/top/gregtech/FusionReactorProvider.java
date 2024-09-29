958442c1e46cbd1e509ab84d8fe6b60d95909609
ko_no <90126004+MrKono@users.noreply.github.com>
2024-09-29 13:46:59 +0900

    add CEu Provider: FusionReactor

---
+++ src/main/java/kono/ceu/gttopaextended/integration/top/gregtech/FusionReactorProvider.java
@@ -0,0 +1,43 @@
+package kono.ceu.gttopaextended.integration.top.gregtech;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.TextFormatting;
+
+import org.jetbrains.annotations.NotNull;
+
+import gregtech.api.capability.IEnergyContainer;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.common.metatileentities.multi.electric.MetaTileEntityFusionReactor;
+import gregtech.integration.theoneprobe.provider.ElectricContainerInfoProvider;
+
+import mcjty.theoneprobe.api.*;
+import mcjty.theoneprobe.apiimpl.elements.ElementProgress;
+
+public class FusionReactorProvider extends ElectricContainerInfoProvider {
+
+    private IEnergyContainer container;
+
+    protected void addProbeInfo(@NotNull IEnergyContainer capability, @NotNull IProbeInfo probeInfo,
+                                EntityPlayer player, @NotNull TileEntity tileEntity, @NotNull IProbeHitData data) {
+        super.addProbeInfo(capability, probeInfo, player, tileEntity, data);
+        if (tileEntity instanceof IGregTechTileEntity) {
+            MetaTileEntity metaTileEntity = ((IGregTechTileEntity) tileEntity).getMetaTileEntity();
+            if (metaTileEntity instanceof MetaTileEntityFusionReactor &&
+                    ((MetaTileEntityFusionReactor) metaTileEntity).isStructureFormed()) {
+                long heat = ((MetaTileEntityFusionReactor) metaTileEntity).getHeat();
+                long capacity = capability.getEnergyCapacity();
+                probeInfo.text(TextFormatting.RED + "{*fusion_reactor.heat*}");
+                probeInfo.progress(heat, capacity, probeInfo.defaultProgressStyle()
+                        .numberFormat(
+                                player.isSneaking() || heat < 10000 ? NumberFormat.FULL : NumberFormat.COMPACT)
+                        .suffix(" / " + (player.isSneaking() || capacity < 10000 ? capacity :
+                                ElementProgress.format(capacity, NumberFormat.COMPACT, "")))
+                        .filledColor(0xFFEEE600)
+                        .alternateFilledColor(0xFFEEE600)
+                        .borderColor(0xFF555555));
+            }
+        }
+    }
+}
