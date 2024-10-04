db60c1e5cf4280a26946de7cd38bbb34df50783d
ko_no <90126004+MrKono@users.noreply.github.com>
2024-10-04 21:00:51 +0900

    fix Duplicated display

--- src/main/java/kono/ceu/gttopaextended/integration/top/gregtech/FusionReactorProvider.java
+++ src/main/java/kono/ceu/gttopaextended/integration/top/gregtech/FusionReactorProvider.java
@@ -1,39 +1,47 @@
 package kono.ceu.gttopaextended.integration.top.gregtech;
 
-import kono.ceu.gttopaextended.Tags;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.capabilities.Capability;
 
 import org.jetbrains.annotations.NotNull;
 
+import gregtech.api.capability.GregtechCapabilities;
 import gregtech.api.capability.IEnergyContainer;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.common.metatileentities.multi.electric.MetaTileEntityFusionReactor;
-import gregtech.integration.theoneprobe.provider.ElectricContainerInfoProvider;
+import gregtech.integration.theoneprobe.provider.CapabilityInfoProvider;
+
+import kono.ceu.gttopaextended.Tags;
 
 import mcjty.theoneprobe.api.*;
 import mcjty.theoneprobe.apiimpl.elements.ElementProgress;
 
-public class FusionReactorProvider extends ElectricContainerInfoProvider {
+public class FusionReactorProvider extends CapabilityInfoProvider<IEnergyContainer> {
 
     @Override
     public String getID() {
         return Tags.MODID + ":gt_fusion_reactor";
     }
 
-    protected void addProbeInfo(@NotNull IEnergyContainer capability, @NotNull IProbeInfo probeInfo,
+    @Override
+    protected @NotNull Capability<IEnergyContainer> getCapability() {
+        return GregtechCapabilities.CAPABILITY_ENERGY_CONTAINER;
+    }
+
+    @Override
+    protected void addProbeInfo(@NotNull IEnergyContainer capability, @NotNull IProbeInfo info,
                                 EntityPlayer player, @NotNull TileEntity tileEntity, @NotNull IProbeHitData data) {
-        super.addProbeInfo(capability, probeInfo, player, tileEntity, data);
         if (tileEntity instanceof IGregTechTileEntity) {
             MetaTileEntity metaTileEntity = ((IGregTechTileEntity) tileEntity).getMetaTileEntity();
             if (metaTileEntity instanceof MetaTileEntityFusionReactor &&
                     ((MetaTileEntityFusionReactor) metaTileEntity).isStructureFormed()) {
                 long heat = ((MetaTileEntityFusionReactor) metaTileEntity).getHeat();
                 long capacity = capability.getEnergyCapacity();
-                probeInfo.text(TextFormatting.RED + "{*gttopadditionextended.fusion_reactor.heat*}");
-                probeInfo.progress(heat, capacity, probeInfo.defaultProgressStyle()
+                info.text(TextFormatting.RED + "{*gttopadditionextended.fusion_reactor.heat*}");
+                info.progress(heat, capacity, info.defaultProgressStyle()
                         .numberFormat(
                                 player.isSneaking() || heat < 10000 ? NumberFormat.FULL : NumberFormat.COMPACT)
                         .suffix(" / " + (player.isSneaking() || capacity < 10000 ? capacity :
